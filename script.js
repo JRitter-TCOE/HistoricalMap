03a96a19ca7b469cf9af53f3d12e7f0504259bdf
JRitter-TCOE <jritter@tcoek12.org>
2024-05-22 17:12:26 -0700

    Testing zoom

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const mapContainer = document.getElementById("map_container");
+const zoomIn = document.getElementById('zoom_in');
+const zoomOut = document.getElementById('zoom_out');
 
-let zoom = 50;
+let zoom = 500;
 
-mapContainer.style.width = `${zoom}vw`;
+mapContainer.style.width = `${zoom}px`;
+
+zoomIn.onclick = () => {
+    zoom += 100;
+    mapContainer.style.width = `${zoom}px`;
+}
+
+zoomOut.onclick = () => {
+    zoom -= 100;
+    mapContainer.style.width = `${zoom}px`;
+}
